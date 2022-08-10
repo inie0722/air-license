818b39e5f4be6b7db308ae35530500eac8e06db8
然Y <inie0722@gmail.com>
2022-08-10 14:00:35 +0800

    添加腾讯云实例id

---
+++ include/air/license/cloud_instance_id.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "air/license/identification.hpp"
+#include "air/license/tool.hpp"
+
+#include <boost/algorithm/string.hpp>
+
+namespace air
+{
+    namespace license
+    {
+        class cloud_instance_id : public identification
+        {
+        public:
+            cloud_instance_id() = default;
+
+            virtual ~cloud_instance_id() = default;
+
+            virtual Json::Value info()
+            {
+                Json::Value ret;
+
+                auto res = tool::system("curl --connect-timeout 1 http://metadata.tencentyun.com/latest/meta-data/instance-id 2>/dev/null");
+
+                if (res.second == 0)
+                {
+                    std::vector<std::string> strs;
+                    boost::split(strs, res.first, boost::is_any_of("\n"));
+
+                    int i = 0;
+                    for (auto &str : strs)
+                    {
+                        if (str.length() > 0)
+                        {
+                            ret[i] = "tencent." + str;
+                            ++i;
+                        }
+                    }
+                }
+
+                return ret;
+            }
+
+            virtual Json::Value generate(const Json::Value &arg)
+            {
+                return arg;
+            }
+
+            virtual bool validate(const Json::Value &arg)
+            {
+                return arg == this->info();
+            }
+        };
+    }
+}
